cfe1ca393b9829c9e169560cf8ebe6aa3e6ddcb1
sampsn <g@samp.sn>
2024-05-20 11:13:08 -0600

    Simplify code, and add "Your total bill is" string

--- main.js
+++ main.js
@@ -4,5 +4,6 @@ const tipPercentage = Number(prompt("What percentage would you like to tip?"));
 // tipPercentage = parseInt(tipPercentage);
 
 const tipAmount = total * (tipPercentage * 0.01);
+const totalAmount = (total + tipAmount).toFixed(2);
 
-alert(Number(total + tipAmount).toFixed(2));
+alert("Your total bill is $" + totalAmount);
